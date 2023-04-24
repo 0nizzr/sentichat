ddae8fd31ceaa8e24ba1d790a8d985b136d93872
0nizzr <nizzrdev@gmail.com>
2023-04-24 12:22:35 +0100

    Refactor ChatHeader component with updated design and connected status indicator

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -8,12 +8,13 @@ export default function ChatHeader() {
 
       <div className="ml-4">
         {/* name */}
-        <h1 className="mx-4 text-3xl cursor-pointer hover:text-cyan-800">
+        <h1 className="text-3xl cursor-pointer hover:text-cyan-800">
           John Doe
         </h1>
         {/* connedted status text + green/gray dot */}
-        <div className=" relative left-4 h-2 w-2 bg-green-600 rounded-full">
-          <h3 className="relative bottom-1 left-3 text-sm">Connected</h3>
+        <div className="flex items-center">
+          <div className="mr-1 h-2 w-2 bg-green-600 rounded-full"></div>
+          <h3 className="text-sm">Connected</h3>
         </div>
       </div>
     </div>
