94a32c853fa7e3f34647c168ab1123e0c26d1405
0nizzr <nizzrdev@gmail.com>
2023-04-25 22:26:56 +0100

    Add hardcoded images with Image component

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,16 +1,20 @@
+import Image from "next/image";
 export default function ChatHeader() {
   return (
     <div className="flex bg-white h-20 px-10 items-center">
       {/* Profil pic */}
-      <div className=" rounded-full h-14 w-14 bg-black cursor-pointer"></div>
-
+      <Image
+        className="rounded-full h-14 w-14 cursor-pointer"
+        src="/bojack.webp"
+        alt="Bruce"
+        width={50}
+        height={50}
+      />
       {/* container name & status */}
 
       <div className="ml-4">
         {/* name */}
-        <h1 className="text-3xl cursor-pointer hover:text-cyan-800">
-          John Doe
-        </h1>
+        <h1 className="text-3xl cursor-pointer hover:text-cyan-800">Bojack</h1>
         {/* connedted status text + green/gray dot */}
         <div className="flex items-center">
           <div className="mr-1 h-2 w-2 bg-green-600 rounded-full"></div>
--- src/components/SideBarFriend.tsx
+++ src/components/SideBarFriend.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 export default function SideBarFriend() {
   return (
     <div className="flex items-center h-20 border-b">
       {/* Profil pic */}
-      <div className="rounded-full h-14 w-14 bg-black cursor-pointer mx-5"></div>
+      <div className=""></div>
+      <Image
+        className="rounded-full h-14 w-14 cursor-pointer mx-5"
+        src="/bruce.png"
+        alt="Bruce"
+        width={50}
+        height={50}
+      />
       <div>
         {/* name */}
         <h1 className="text-xl cursor-pointer hover:text-cyan-800">
@@ -12,7 +20,6 @@ export default function SideBarFriend() {
         <p className="text-[0.85rem] text-gray-600">
           yo borrowed the batmobile brb
         </p>
-
       </div>
     </div>
   );
